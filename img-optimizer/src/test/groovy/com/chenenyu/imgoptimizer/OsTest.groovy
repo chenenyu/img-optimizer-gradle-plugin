0a37aaa7ecf7299cda88705e702adb5efdf3fdda
chenenyu <echocey@gmail.com>
2016-07-13 18:41:13 +0800

    add test case

---
+++ img-optimizer/src/test/groovy/com/chenenyu/imgoptimizer/OsTest.groovy
@@ -0,0 +1,13 @@
+package com.chenenyu.imgoptimizer
+
+import com.chenenyu.imgoptimizer.util.PngquantUtil
+
+/**
+ * @Author: chenenyu
+ * @Created: 16/7/13 18:27.
+ */
+class OsTest {
+    static void main(String[] args) {
+        println(PngquantUtil.getFilename())
+    }
+}
